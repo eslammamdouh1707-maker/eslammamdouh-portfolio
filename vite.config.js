ac7f2e2566148acd159f76720dd7aa527d4f7730
Eslam Mamdouh <mamdouheslam913@gmail.com>
2025-10-27 10:05:55 +0300

    Set main page to public/portfolio.html

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/eslammamdouh-portfolio/', // ✅ خليه كده بالضبط
+  base: './',
   build: {
-    outDir: 'dist'
-  },
-  server: {
-    host: true
+    outDir: 'dist',
   }
 })
 
